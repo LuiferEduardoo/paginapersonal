926a4cfbf2938411ef3a4cc36958e897511beb9a
Luifer Ortega <luifer01ortegaperez@gmail.com>
2022-11-29 15:29:50 -0500

    Se corrigen algunos errores e integra .htaccess

--- Php/contacto.php
+++ Php/contacto.php
@@ -1,15 +1,24 @@
-$nombre = $ _POST['nombre'];
-$email = $ _POST['email'];
-$mensaje = $ _POST['mensaje'];
-$para = 'luifer01ortegaperez@gmail.com';
-$titulo = 'Nuevo correo de la web;
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+$para = 'contacto@luifereduardoo.com';
+$titulo = 'Nuevo correo de la web';
  
 $msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
  
-if ($ _POST['submit']) {
+if ($_POST['submit'])
+{
     if (mail ($para, $titulo, $msjCorreo)) {
-    echo 'El mensaje se ha enviado';
-} else {
-echo 'Falló el envio';
+    echo '<script> alert ("El mensaje se ha enviado"); 
+    window.location.replace("http://luifereduardoo.com/contacto.html");
+    </script>';
+} 
+else {
+    echo '<script> alert ("Se ha producido un error, por favor intente de nuevo");
+    window.location.replace("http://luifereduardoo.com/contacto.html");
+    </script>';
 }
 }
+
+?>
