68c62fb5821c60f66fd604a6ceb645bb05f8e944
Luifer Ortega <luifer01ortegaperez@gmail.com>
2023-01-08 21:41:11 -0500

    Solución de bug

--- contacto.php
+++ contacto.php
@@ -1,3 +1,32 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+$para = 'contacto@luifereduardoo.com';
+$titulo = 'Nuevo correo de la web';
+ 
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
+ 
+if ($_POST['submit'])
+{
+    if (mail ($para, $titulo, $msjCorreo)) {
+        ?>
+        <div class="message">
+            <p>¡El mensaje se ha enviado con exito! Muy pronto respondere su mensaje.</p>
+            <img class = "message_img" src="https://cdn-icons-png.flaticon.com/512/1828/1828778.png" alt="cerrar" srcset="">
+        </div>
+        <?php
+    } 
+    else {
+        ?>
+        <div class="message_error">
+            <p>Se ha producido un error a la hora de enviar el mensaje. Intente de nuevo, o comuniquese directamente a el siguiente correo: <br><a href="mailto:contacto@luifereduardoo.com">contacto@luifereduardoo.com</a></p>
+            <img class = "message_error_img" src="https://cdn-icons-png.flaticon.com/512/1828/1828778.png" alt="cerrar" srcset="">
+        </div>
+        <?php
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
   <head>
@@ -60,7 +89,7 @@
     </nav>
       <section class="principal">
         <h1>CONTACTAME</h1>
-        <form class="principal--formulario-contacto" method="post" action="php/contacto" onsubmit="return validarFormulario()">
+        <form class="principal--formulario-contacto" method="post" action="#" onsubmit="return validarFormulario()">
             <label for="nombre">Nombre:</label>
             <input id="nombre" name="nombre" placeholder="Nombre completo" required>
             <label for="email">Email:</label>
