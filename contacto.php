1b58a6ad15dc47c36159adbcf7d38ff4b50afed2
Luifer Ortega <luifer01ortegaperez@gmail.com>
2023-03-18 13:27:21 -0500

    se elimina sobre-mi.html y otros cambios

--- contacto.php
+++ contacto.php
@@ -35,59 +35,52 @@ if ($_POST['submit'])
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contactame</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="Css/Contacto/contacto.css">
     <link rel="stylesheet" href="Css/Menu/menu.css">
     <link rel="stylesheet" href="Css/Footer/footer.css">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <link rel="icon" type="image/png" href="https://i.ibb.co/pnb03Fv/Likepng.png"/>
   </head>
   <body>
     <nav class="menu">
-      <div class="menu_MobileIcon">
-        <img src="https://i.ibb.co/B2HNMRx/icons8-men-redondeado-32.png" alt="">
-      </div>
-        <a href="index" class="menu_logo">
-          <img src="https://i.ibb.co/mJQxt3j/Logotipo-white.png"class="logo" alt="Logo" srcset="">
-        </a>
-      <ul class="menu_Desktop">
-        <li>
-          <a href="index">HOME</a>
-        </li>
-        <li>
-          <a href="sobre-mi">SOBRE MÍ</a>
-        </li>
-        <li>
-          <a href="portaforio">PORTAFORIO</a>
-        </li>
-        <li>
-          <a href="blog.php">BLOG</a>
-        </li>
-        <li>
-          <a href="contacto">CONTACTO</a>
-        </li>
-      </ul>
-      <div class="menu_Mobile" >
-        <ul>
+        <div class="menu_MobileIcon">
+          <img src="https://i.ibb.co/B2HNMRx/icons8-men-redondeado-32.png" alt="">
+        </div>
+          <a href="index" class="menu_logo">
+            <img src="img/Iconos/logo-black.png"class="logo" alt="Logo" srcset="">
+          </a>
+        <ul class="menu_Desktop">
           <li>
-            <a href="index"><img src="https://img.icons8.com/wired/64/null/home-page.png" alt="" srcset="">HOME </a>
+            <a href="index">Home</i></a>
           </li>
           <li>
-            <a href="sobre-mi"><img src="https://cdn-icons-png.flaticon.com/512/1443/1443781.png" alt="" srcset="">SOBRE MÍ</a>
+            <a href="portafolio">Portafolio</a>
           </li>
           <li>
-            <a href="portaforio"><img src="https://cdn-icons-png.flaticon.com/512/943/943329.png" alt="" srcset="">PORTAFORIO</a>
+            <a href="blog.php">Blog</a>
           </li>
           <li>
-            <a href="blog.php"><img src="https://cdn-icons-png.flaticon.com/512/4922/4922073.png" alt="">BLOG</a>
-          </li>
-          <li>
-            <a href="contacto"><img src="https://cdn-icons-png.flaticon.com/512/2590/2590818.png" alt="" srcset="">CONTACTO</a>
+            <a href="contacto">Contacto</a>
           </li>
         </ul>
-      </div>
-    </nav>
+        <div class="menu_Mobile" >
+          <i class="las la-times close"></i>
+          <ul>
+            <li>
+              <a href="index"><i class="las la-home icon"></i></i>Home</a>
+            </li>
+            <li>
+              <a href="portafolio"><i class="las la-briefcase icon"></i>Portafolio</a>
+            </li>
+            <li>
+              <a href="blog.php"><i class="las la-blog icon"></i></i>Blog</a>
+            </li>
+            <li>
+              <a href="contacto"><i class="las la-envelope-open-text icon"></i>Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
       <section class="principal">
         <h1>CONTACTAME</h1>
         <form class="principal--formulario-contacto" method="post" action="#" onsubmit="return validarFormulario()">
@@ -101,39 +94,59 @@ if ($_POST['submit'])
         </form>
       </section>
       <footer>
-        <div class="footer--conteiner">
-          <section class="footer--conteiner__correo">
-            <p>CORREO</p>
-            <img src="https://i.ibb.co/8609h4Y/correo.png" alt="Correo">
-            <a href="mailto:contacto@luifereduardoo.com">contacto@luifereduardoo.com</a>
+        <div class="footer-conteiner">
+          <section class="footer-conteiner-infor-mobile">
+            <div class="footer-conteiner-infor-logo">
+              <img src="img/Iconos/logo-white.png" alt="Logo" srcset="">
+            </div>
+            <div class="footer-conteiner-infor-social-media">
+              <a href="https://www.twitter.com/luifereduardoo" target="_blank"><i class="lab la-twitter"></i></a>
+              <a href="https://www.instagram.com/luifereduardoo" target="_blank"><i class="lab la-instagram"></i></a>
+              <a href="https://www.facebook.com/luifereduardoo" target="_blank"><i class="lab la-facebook"></i></a>
+              <a href="https://www.linkedin.com/in/luifereduardoo/" target="_blank"><i class="lab la-linkedin-in"></i></a>
+              <a href="https://github.com/LuiferEduardoo" target="_blank"><i class="lab la-github"></i></a>
+            </div>
           </section>
-          <section class="footer--conteiner__enlaces">
+          <section class="footer-conteiner-contact">
+            <p>CORREO</p>
+            <div class="footer-container-contact-email">
+              <i class="las la-envelope-open"></i>
+              <a href="mailto:contacto@luifereduardoo.com">contacto@luifereduardoo.com</a>
+            </div>
+          </section>
+          <section class="footer-conteiner-infor">
+            <div class="footer-conteiner-infor-logo">
+              <img src="img/Iconos/logo-white.png" alt="Logo" srcset="">
+            </div>
+            <div class="footer-conteiner-infor-social-media">
+              <a href="https://www.twitter.com/luifereduardoo" target="_blank"><i class="lab la-twitter"></i></a>
+              <a href="https://www.instagram.com/luifereduardoo" target="_blank"><i class="lab la-instagram"></i></a>
+              <a href="https://www.facebook.com/luifereduardoo" target="_blank"><i class="lab la-facebook"></i></a>
+              <a href="https://www.linkedin.com/in/luifereduardoo/" target="_blank"><i class="lab la-linkedin-in"></i></a>
+              <a href="https://github.com/LuiferEduardoo" target="_blank"><i class="lab la-github"></i></a>
+            </div>
+          </section>
+          <section class="footer-conteiner-link">
             <p>ENLACES</p>
             <ul>
               <li>
-                <a href="sobre-mi"> SOBRE MÍ</a>
+                <a href="sobre-mi">Home</a>
               </li>
               <li>
-                <a href="portaforio">PORTAFORIO</a>
+                <a href="portafolio">Portafolio</a>
               </li>
               <li>
-                <a href="blog.php">BLOG</a>
+                <a href="blog.php">Blog</a>
               </li>
               <li>
-                <a href="contacto">CONTACTO</a>
+                <a href="contacto">Contacto</a>
               </li>
             </ul>
           </section>
-          <section class="footer--conteiner__redes-sociales">
-            <p>REDES SOCIALES</p>
-            <a href="https://www.twitter.com/luifereduardoo"><img src="https://i.ibb.co/kXbdQmC/twitter.png" alt="Twitter"></a>
-            <a href="https://www.instagram.com/luifereduardoo"><img src="https://i.ibb.co/F812NGB/instagram.png" alt="Instagram"></a>
-            <a href="https://www.facebook.com/luifereduardoo"><img src="https://i.ibb.co/fn6TjWL/facebook.png" alt="Facebook"></a>
-            <a href="https://www.linkedin.com/in/luifereduardoo/"><img src="https://i.ibb.co/kxtgH68/linkedin.png" alt="Linkedin"></a>
-            <a href="https://github.com/LuiferEduardoo"><img src="https://i.ibb.co/M9dYPNB/github.png" alt="Github"></a>
-          </section>
         </div>
-        <p class="footer--creditos">Luifer Eduardo Ortega © 2022</p>
+        <div class="footer-copyright">
+          <p>Luifer Eduardo Ortega ©2023</p>
+        </div>
       </footer>
     <script src="Scripts\Contacto/contacto.js"></script>
     <script src="Scripts/nav/nav.js"></script>
