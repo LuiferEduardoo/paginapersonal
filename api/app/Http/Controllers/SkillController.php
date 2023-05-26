378216a5bb52cedac6c0caf63fb85e16ab18a6b4
Luifer Ortega <luifer01ortegaperez@gmail.com>
2023-05-25 20:50:55 -0500

    Refactorización de los endpoints de skills

--- api/app/Http/Controllers/SkillController.php
+++ api/app/Http/Controllers/SkillController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Skills;
-use App\Models\RegistrationOfImages;
-use App\Services\ImageService;
+use App\Services\ImageAssociationService;
 use App\Services\TagsService;
 use App\Http\Requests\ValidateDate;
 use Illuminate\Support\Facades\Storage;
@@ -15,13 +14,13 @@ use Illuminate\Database\QueryException;
 
 class SkillController extends Controller
 {
-    protected $imageService;
     protected $tagsService;
+    protected $imageAssociationService;
 
-    public function __construct(ImageService $imageService, TagsService $tagsService)
+    public function __construct(TagsService $tagsService, ImageAssociationService $imageAssociationService)
     {
-        $this->imageService = $imageService;
         $this->tagsService = $tagsService;
+        $this->imageAssociationService = $imageAssociationService;
     }
 
     public function GetSkills(Request $request){
@@ -62,37 +61,22 @@ class SkillController extends Controller
 
             if($request->hasFile('image')){
                 $file = $request->file('image');
-                // Se guarda la imagen en la API
-                $uploadImage = $this->imageService->saveImage($file, 'skill', $token);
-                // Asocia la imagen con la habilidad
-                $skill->RelationImage()->attach($uploadImage);  
+                // Se guarda la imagen en la API y se hace la asociación
+                $this->imageAssociationService->saveImage($skill, $file, 'skill', 'RelationImage', $token);  
             }else if($request->input('id_image')){
                 $idImage =$request->input('id_image');
-                $imageForId = RegistrationOfImages::find($idImage);
-                $isRemoved = $imageForId['removed_at'];
-                if ($imageForId && !$isRemoved){
-                    $skill->RelationImage()->attach($idImage);
-                } else{
-                    DB::rollBack();
-                    return response()->json([
-                        'message' => "Image not fount"
-                    ], 404);
-                }
+                $this->imageAssociationService->saveImageForId($skill, $idImage, 'RelationImage');
             }else if($request->input('url')){
                 $url = $request->input('url');
-                $skill->url = $url;
-                $skill->save();
+                $this->imageAssociationService->saveImageForUrl($skill, $url);
             } else{
                 DB::rollBack();
                     return response()->json([
                         'message' => "Image not entered"
                     ], 400);
             }
-            // Se obtiene los IDs de los tags
-            $tagIds = $this->tagsService->createAndAttachTags($arrayTags);
-
-            // Asocia los tags con la habilidad
-            $skill->RelationTags()->attach($tagIds);
+            // Se crean los tags y se asocian con la la habilidad
+            $this->tagsService->createTags($skill, $arrayTags,'RelationTags');
 
             DB::commit(); // Confirmar la transacción
             return response()->json([
@@ -118,18 +102,12 @@ class SkillController extends Controller
 
             $token = $request->header('Authorization');
             $token = str_replace('Bearer ', '', $token);
+            $eliminateImage = filter_var($request->input('eliminate_image'), FILTER_VALIDATE_BOOLEAN);
 
             if(Skills::findOrFail($id)){
                 $skill = Skills::findOrFail($id);
-                $tagIds = $skill->tags->pluck('id');
-                $imageIArray = $skill->RelationImage()->pluck('image_id');
-                $skill->RelationTags()->detach($tagIds);
-                $skill->RelationImage()->detach();
-                $eliminateImage = filter_var($request->input('eliminate_image'), FILTER_VALIDATE_BOOLEAN);
-                if($eliminateImage){
-                    $imageId = $imageIArray[0];
-                    $deleteImage = $this->imageService->deleteImage($imageId, $token);
-                }
+                $this->tagsService->deleteTags($skill, 'RelationTags');
+                $this->imageAssociationService->deleteImage($skill, 'RelationImage', $eliminateImage, $token);
                 $skill->delete();
                 return response()->json(['message' => 'Skills successfully deleted'],200);
             }
@@ -162,70 +140,26 @@ class SkillController extends Controller
             $skill->save();
             // Se convierten los tags en array
             $arrayTags = explode(",", $tags); 
+            // Se actualizan los tags
+            $this->tagsService->updateTags($skill, $arrayTags, 'RelationTags');
 
             $replaceImage = filter_var($request->input('replace_image'), FILTER_VALIDATE_BOOLEAN);
 
             if($request->hasFile('image')){
                 $file = $request->file('image');
-                if($skill->url != Null){
-                    $skill->url = Null;
-                    $skill->save();
-                }
-                if($replaceImage){
-                    // Se reemplaza la imagen
-                    $idImageExit = $skill->RelationImage()->first()->id;
-                    $this->imageService->updateImage($idImageExit, $file, $token);
-                }else{
-                    // Se sube la imagen
-                    $updateImage = $this->imageService->saveImage($file, 'skill', $token);
-                    // Actualiza la relación de imagen en la habilidad
-                    $skill->RelationImage()->sync([$updateImage]);
-                }
+                $this->imageAssociationService->updateImage($skill, $file, $replaceImage, 'RelationImage', 'skill', $token);
             } else if($request->input('id_image')){
                 $idImage =$request->input('id_image');
-                if($skill->url != Null){
-                    $skill->url = Null;
-                    $skill->save();
-                }
-                $imageForId = RegistrationOfImages::find($idImage);
-                $isRemoved = $imageForId['removed_at'];
-                if ($imageForId && !$isRemoved){
-                    if($replaceImage){
-                        $idImageExit = $skill->RelationImage()->first()->id;
-                        $this->imageService->deleteImage($idImageExit, $token);
-                    }
-                    $skill->RelationImage()->sync([$idImage]);
-                } else{
-                    DB::rollBack(); // Deshacer la transacción en caso de error
-                    return response()->json([
-                        'message' => "Image not fount"
-                    ], 404);
-                }
+                $this->imageAssociationService->updateImageForId($skill, $idImage, $replaceImage, 'RelationImage', $token);
             } else if($request->input('url')){
                 $url = $request->input('url');
-                $skill->url = $url;
-                $skill->save();
-                if($replaceImage){
-                    $idImageExit = $skill->RelationImage()->first()->id;
-                    $this->imageService->deleteImage($idImageExit, $token);
-                }
-                if($skill->RelationImage()->first()->id){
-                    $skill->RelationImage()->detach();
-                }
+                $this->imageAssociationService->updateImageForUrl($skill, $url, $replaceImage, 'RelationImage', $token);
             }else{
                 DB::rollBack();
                     return response()->json([
                         'message' => "Image not entered"
                     ], 400);
             }
-            // Se obtiene los IDs de los tags
-            $tagIds = $this->tagsService->createAndAttachTags($arrayTags);
-
-            // Eliminar las asociaciones existentes
-            $skill->RelationTags()->detach();
-
-            // Asocia los tags con las nuevas habilidad
-            $skill->RelationTags()->attach($tagIds);
 
             DB::commit(); // Confirmar la transacción
         
@@ -273,35 +207,13 @@ class SkillController extends Controller
             if($request->input('tags')){
                 $tags = $request->input('tags');
                 $arrayTags = explode(",", $tags); 
-                // Se obtiene los IDs de los tags
-                $tagIds = $this->tagsService->createAndAttachTags($arrayTags);
-
-                // Eliminar las asociaciones existentes
-                $skill->RelationTags()->detach();
-
-                // Asocia los tags con las nuevas habilidad
-                $skill->RelationTags()->attach($tagIds);
+                $this->tagsService->updateTags($skill, $arrayTags, 'RelationTags');
             }
             if($request->file('image')){
                 $image = $request->file('image'); 
-                if($skill->url != Null){
-                    $skill->url = Null;
-                    $skill->save();
-                }
                 if(!$request->input('url') && !$request->input('id_image')){
-
-                    if($replaceImage){
-                        // Se reemplaza la imagen
-                        $idImageExit = $skill->RelationImage()->first()->id;
-                        $this->imageService->updateImage($idImageExit, $image, $token);
-                    }else{
-                        // Se sube la imagen
-                        $updateImage = $this->imageService->saveImage($image, 'skill', $token);
-                        // Actualiza la relación de imagen en la habilidad
-                        $skill->RelationImage()->sync([$updateImage]);
-                    }
-                }
-                else{
+                    $this->imageAssociationService->updateImage($skill, $image, $replaceImage, 'RelationImage', 'skill', $token);
+                } else{
                     DB::rollBack();
                     return $errorImage;
                 }
@@ -309,15 +221,7 @@ class SkillController extends Controller
             if($request->input('url')){
                 if(!$request->file('image') && !$request->input('id_image')){
                     $url = $request->input('url');
-                    $skill->url = $url;
-                    $skill->save();
-                    if($replaceImage){
-                        $idImageExit = $skill->RelationImage()->first()->id;
-                        $this->imageService->deleteImage($idImageExit, $token);
-                    }
-                    if($skill->RelationImage()->first()->id){
-                        $skill->RelationImage()->detach();
-                    }
+                    $this->imageAssociationService->updateImageForUrl($skill, $url, $replaceImage, 'RelationImage', $token);
                 }
                 else{
                     DB::rollBack();
@@ -327,24 +231,7 @@ class SkillController extends Controller
             if($request->input('id_image')){
                 $idImage = $request->input('id_image');
                 if(!$request->file('image') && !$request->input('url')){
-                    if($skill->url != Null){
-                        $skill->url = Null;
-                        $skill->save();
-                    }
-                    $imageForId = RegistrationOfImages::find($idImage);
-                    $isRemoved = $imageForId['removed_at'];
-                    if ($imageForId && !$isRemoved){
-                        if($replaceImage){
-                            $idImageExit = $skill->RelationImage()->first()->id;
-                            $this->imageService->deleteImage($idImageExit, $token);
-                        }
-                        $skill->RelationImage()->sync([$idImage]);
-                    } else{
-                        DB::rollBack(); // Deshacer la transacción en caso de error
-                        return response()->json([
-                            'message' => "Image not fount"
-                        ], 404);
-                    }
+                    $this->imageAssociationService->updateImageForId($skill, $idImage, $replaceImage, 'RelationImage', $token);
                 }
                 else{
                     DB::rollBack();
