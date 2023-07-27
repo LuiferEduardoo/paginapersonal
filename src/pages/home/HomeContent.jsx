b0ce4035b629f283d46f2df4e6c5713be596d290
Luifer Ortega <luifer01ortegaperez@gmail.com>
2023-07-26 22:48:03 -0500

    Mejorar información personal:
    - Actualizada la descripción de mi perfil con detalles más precisos y completos.
    - Mejorada la presentación y formato general para mayor claridad.
    #profile #information #Improvement

--- src/pages/home/HomeContent.jsx
+++ src/pages/home/HomeContent.jsx
@@ -38,11 +38,13 @@ const HomeContent = () => {
                 <div className={styles.mainContainerAbout} ref={myComponent}>
                     <section className={styles.mainContainerAboutText}>
                         <h1>Sobre mí</h1>
-                        <p>Mi nombre es Luifer Eduardo Ortega Pérez, tengo 17 años y desde pequeño mi gran pasión es la tecnología.</p>
-                        <p>A los 13 años aprendí a programar imprimiendo mi primer "Hola mundo" gracias a mi curiosidad y mi capacidad autodidacta.</p>
-                        <p>Aunque sé programar en lenguajes como: JavaScript, Python y PHP.</p>
-                        Actualmente, me encuentro ampliando mis conocimientos en estas áreas. Aparte de la gran pasión que le tengo a la tecnología, también tengo un enorme amor a la escritura, por eso es que cuentan con la sección de <a href="/blog">Blog</a> donde podrán ver mis redacciones.
-                        <p>¡No hay nada imposible, solo cosas difíciles!</p>
+                        <p>¡Hola! Mi nombre es Luifer Eduardo Ortega Pérez, y a mis 17 años, me apasiona profundamente la tecnología. Desde que era pequeño, he sentido una curiosidad innata por entender cómo funcionan las cosas y una habilidad autodidacta que me ha permitido aprender de forma independiente.</p>
+                        <p>A la temprana edad de 13 años, di mis primeros pasos en la programación con una sencilla impresión de "Hola mundo". Fue ese pequeño logro el que despertó en mí una pasión desbordante por la informática y la programación.</p>
+                        <p>En el ámbito del desarrollo web, me siento cómodo trabajando con distintos lenguajes como JavaScript, Python y PHP. Mis conocimientos me permiten crear páginas interactivas y dinámicas, lo que me fascina, ya que puedo dar vida a mis ideas y proyectos.</p>
+                        <p>Además de mi amor por la tecnología, tengo una profunda pasión por la escritura. Es por eso que cuento con una sección de Blog, donde puedo compartir mis pensamientos, conocimientos y experiencias con el mundo. Creo firmemente que la combinación de la tecnología y la escritura me da una ventaja única para expresar mis ideas y aportar valor a la comunidad.</p>
+                        <p>Un lema que siempre me ha inspirado es: "No hay nada imposible, solo cosas difíciles". Con esta mentalidad, enfrento nuevos desafíos en el mundo de la tecnología y la programación, siempre dispuesto a aprender y mejorar.</p>
+                        <p>En resumen, soy un apasionado de la tecnología, con habilidades en el desarrollo frontend utilizando ReactJS y en el manejo de bases de datos como SQL. En el backend, mi experiencia se basa en el uso de Node.js, lo que me permite crear aplicaciones web completas y robustas.</p>
+                        <p>Estoy emocionado por seguir creciendo en este apasionante mundo, enfrentando nuevos retos y contribuyendo al avance de la tecnología. Siempre dispuesto a aprender y compartir mis conocimientos, estoy seguro de que puedo aportar mucho valor a cualquier proyecto en el que participe. ¡El futuro es emocionante y estoy listo para aprovechar al máximo cada oportunidad que se presente!</p>
                     </section>
                         <section className={styles.mainContainerAboutImg}>
                             <LazyLoadImage src="https://cdn.luifereduardoo.com/img/about/img-1.webp" alt="Img-about-me" />
