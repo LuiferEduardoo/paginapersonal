a7b988ea81b661f8b662e6b5f73c5ccf573390ae
Luifer <luifer01ortegaperez@gmail.com>
2024-08-20 22:09:03 -0500

    fix(api): corregir redondeo incorrecto del tiempo en la clase Time

--- api/app/utils/Time.php
+++ api/app/utils/Time.php
@@ -3,11 +3,10 @@
 namespace App\Utils;
 
 class Time {
-    public function readingTime($content){
+    public function readingTime($content) {
         $numberWords = str_word_count(strip_tags($content));
-        $readingTimeNoForm = ceil($numberWords / 200);
-        $readingTimeHours = floor($readingTimeNoForm/60); 
-        $readingTimeMinutes = $readingTimeNoForm % 60;
-        return gmdate('H:i:s', mktime($readingTimeHours, $readingTimeMinutes, 0, 0, 0, 0));
+        $readingTimeMinutes = floor($numberWords / 200); // Minutos enteros
+        $readingTimeSeconds = round(($numberWords % 200) / (200 / 60)); // Segundos fraccionarios
+        return gmdate('H:i:s', mktime(0, $readingTimeMinutes, $readingTimeSeconds, 0, 0, 0));
     }
 }
