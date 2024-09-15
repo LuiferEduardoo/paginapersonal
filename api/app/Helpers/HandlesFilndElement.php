13fee3c5eef57c287de383b3e8f82eb798bb9050
Luifer <luifer01ortegaperez@gmail.com>
2024-09-14 22:43:56 -0500

    feat(api/helpers/HandlesFindElement ): agregar propiedad findAll en para centralizar lógica

--- api/app/Helpers/HandlesFilndElement.php
+++ api/app/Helpers/HandlesFilndElement.php
@@ -4,11 +4,16 @@ namespace App\Helpers;
 
 use Illuminate\Database\Eloquent\Model;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use App\Helpers\HandleParameter;
 
 class HandlesFilndElement
 {
 
     protected $handleParameter;
+
+    public function __construct(HandleParameter $handleParameter) {
+        $this->handleParameter = $handleParameter;
+    }
 
     public function findOne(string $modelClass, int $id, array $relations): ?Model
     {
@@ -24,4 +29,37 @@ class HandlesFilndElement
 
         return $model;
     }
+
+    public function findAll(string $modelClass, array $relations, array $query = []): ?array
+    {
+        $model = $modelClass::query();
+
+        if (!empty($relations) && is_array($relations)) {
+            $model = $model->with($relations);
+        }
+
+        $model->where('visible', true);
+        $model->orderBy('created_at', 'desc');
+
+        
+        $pagination = $this->handleParameter->queryParameterPagination($query);
+        $limit = $pagination['limit'];
+        $offset = $pagination['offset'];
+        
+        
+        $data = $model->limit($limit)->offset($offset)->get();
+
+        $total = $model->count();
+        $totalPages = (int) ceil($total / $limit);
+        $currentPage = (int) ceil(($offset + 1) / $limit);
+
+        return [
+            'totalPages' => $totalPages,
+            'currentPage' => $currentPage,
+            'total' => $total,
+            'perPage' => $limit,
+            'data' => $data,
+        ];
+    }
+
 }
